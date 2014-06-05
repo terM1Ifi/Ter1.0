08d15096d7b0880f0c67c6ac57cf434af0781b6d
Mohamed <m.abdeljelil70@gmail.com>
2014-06-05 10:59:31 +0200

    video-chat

---
+++ Video-chat/signaler.js
@@ -0,0 +1,95 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+app.configure(function () {
+    var hourMs = 1000 * 60 * 60;
+    app.use(express.static('static', {
+        maxAge: hourMs
+    }));
+    app.use(express.directory('static'));
+    app.use(express.errorHandler());
+});
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(8088);
+
+// ----------------------------------socket.io
+
+var channels = {};
+var usernames = {};
+
+// sometimes it helps!
+// io.set('transports', ['xhr-polling']);
+
+io.sockets.on('connection', function (socket) {
+    var initiatorChannel = '';
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+
+    socket.on('new-channel', function (data) {
+        if (!channels[data.channel]) {
+            initiatorChannel = data.channel;
+        }
+
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('sendchat', function (data) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	socket.on('adduser', function(username){
+		// we store the username in the socket session for this client
+		socket.username = username;
+		socket.emit()
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
+});
+
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+
+        socket.on('message', function (data) {
+            if (data.sender == sender)
+                socket.broadcast.emit('message', data.data);
+        });
+    });
+}
